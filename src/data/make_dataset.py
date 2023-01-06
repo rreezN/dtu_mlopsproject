882769ed3f6db9c81d4b6b559e4f6723328c6869
s194249 <s194249@student.dtu.dk>
2023-01-06 15:18:42 +0100

    pep8 compliance

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-import torch
 import pickle
 from glob import glob
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+
+import click
 import torch
+from dotenv import find_dotenv, load_dotenv
 from PIL import Image
-from torchvision import transforms, utils
-import numpy as np
+from torchvision import transforms
 from tqdm import tqdm
+
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 @click.argument('image_shape', default=224, type=click.Path())
 @click.argument('norm_strat', default='model', type=click.Path())
-def main(input_filepath: str, output_filepath: str, image_shape: int, norm_strat: str) -> None:
+def main(
+        input_filepath: str,
+        output_filepath: str,
+        image_shape: int,
+        norm_strat: str) -> None:
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
@@ -30,17 +34,17 @@ def main(input_filepath: str, output_filepath: str, image_shape: int, norm_strat
     ])
 
     # Datasets to iterate through
-    datasets = ["training_data", "testing_data", "validation_data", "interesting_data"]
-
-
+    datasets = [
+        "training_data", "testing_data", "validation_data", "interesting_data"
+    ]
 
     # Perform dataset loop
     for dataset in datasets:
-        # Variables in which images and labels are saved in an intermediate step
+        # Variables in which images and labels are saved intermediately
         images = []
         labels = []
         # Iterate through all animals
-        for class_idx, animal in enumerate(glob(input_filepath + f"/{dataset}/*")):
+        for c_idx, animal in enumerate(glob(input_filepath + f"/{dataset}/*")):
             print(f"animal: {animal}")
             # iterte through all animal images
             for file in tqdm(glob(f'{animal}/*')):
@@ -50,7 +54,7 @@ def main(input_filepath: str, output_filepath: str, image_shape: int, norm_strat
                 if image.mode != "RGB":
                     image = image.convert("RGB")
                 # Add image to intermediate step variables
-                labels.append(class_idx)
+                labels.append(c_idx)
                 images.append(transform_to_tensor(image))
 
         # Stack all images into a tensor of tensors
@@ -60,22 +64,25 @@ def main(input_filepath: str, output_filepath: str, image_shape: int, norm_strat
         if norm_strat == 'model':
             # transformer based on pre-trained model, mean and std
             T = transforms.Compose([
-                transforms.Normalize(mean=torch.tensor([0.485, 0.456, 0.406]), std=torch.tensor([0.229, 0.224, 0.225]))
+                transforms.Normalize(
+                    mean=torch.tensor([0.485, 0.456, 0.406]),
+                    std=torch.tensor([0.229, 0.224, 0.225])
+                )
             ])
         else:
             # transformer based on caluclated means and stds.
-            means = images.mean(dim=(0,2,3))
-            stds = images.std(dim=(0,2,3))
+            means = images.mean(dim=(0, 2, 3))
+            stds = images.std(dim=(0, 2, 3))
             T = transforms.Compose([
                 transforms.Normalize(mean=means, std=stds)
             ])
-
-        # Transform images 
+        # Transform images
         norm_images = T(images)
         torch_labels = torch.Tensor(labels)
 
         with open(output_filepath + f"/{dataset}.pickle", "wb") as fp:
             pickle.dump((norm_images, torch_labels), fp)
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,27 +1,26 @@
+import logging
+import os
+import pickle
+from typing import Tuple
+
+import hydra
+import matplotlib.pyplot as plt
 import timm
 import torch
-
-import pickle
-import os
-import click
-import matplotlib.pyplot as plt
-import torch
-import hydra
-import logging
-log = logging.getLogger(__name__)
-
 from torch.utils.data import DataLoader, Dataset
+
+log = logging.getLogger(__name__)
 
 
 class dataset(Dataset):
-    def __init__(self, images, labels):
+    def __init__(self, images: torch.Tensor, labels: torch.Tensor) -> None:
         self.data = images.view(-1, 1, 28, 28)
         self.labels = labels
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: int) -> Tuple[torch.Tensor, torch.Tensor]:
         return self.data[item].float(), self.labels[item]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
 
 
@@ -29,29 +28,37 @@ class dataset(Dataset):
 def train(cfg) -> None:
     log.info("Training day and night")
     model_hparams = cfg.model
-    training_hparams = cfg.training
+    train_hparams = cfg.training
 
-    log.info(training_hparams.hyperparameters.lr)
-    torch.manual_seed(training_hparams.hyperparameters.seed)
+    log.info(train_hparams.hyperparameters.lr)
+    torch.manual_seed(train_hparams.hyperparameters.seed)
 
-    model = timm.create_model(model_hparams.hyperparameters.model_name,
-                              pretrained=model_hparams.hyperparameters.pretrained,
-                              in_chans=model_hparams.hyperparameters.in_chans,
-                              num_classes=model_hparams.hyperparameters.num_classes)
+    model = timm.create_model(
+        model_hparams.hyperparameters.model_name,
+        pretrained=model_hparams.hyperparameters.pretrained,
+        in_chans=model_hparams.hyperparameters.in_chans,
+        num_classes=model_hparams.hyperparameters.num_classes
+    )
 
-    device = "cuda" if training_hparams.hyperparameters.cuda else "cpu"
+    device = "cuda" if train_hparams.hyperparameters.cuda else "cpu"
     log.info(f"device: {device}")
     model = model.to(device)
 
-    with open(training_hparams.hyperparameters.train_data_path, 'rb') as handle:
+    with open(train_hparams.hyperparameters.train_data_path, 'rb') as handle:
         raw_data = pickle.load(handle)
 
     data = dataset(raw_data['images'], raw_data['labels'])
-    dataloader = DataLoader(data, batch_size=training_hparams.hyperparameters.batch_size)
-    optimizer = torch.optim.Adam(model.parameters(), lr=training_hparams.hyperparameters.lr)
+    dataloader = DataLoader(
+        data,
+        batch_size=train_hparams.hyperparameters.batch_size
+    )
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=train_hparams.hyperparameters.lr
+    )
     criterion = torch.nn.CrossEntropyLoss()
 
-    n_epoch = training_hparams.hyperparameters.epochs
+    n_epoch = train_hparams.hyperparameters.epochs
     loss_tracker = []
 
     for epoch in range(n_epoch):
@@ -65,7 +72,9 @@ def train(cfg) -> None:
             optimizer.step()
             running_loss += loss.item()
         loss_tracker.append(running_loss/len(dataloader))
-        log.info(f"Epoch {epoch + 1}/{n_epoch}. Loss: {running_loss/len(dataloader)}")
+        log.info(
+            f"Epoch {epoch+1}/{n_epoch}. Loss: {running_loss/len(dataloader)}"
+        )
     torch.save(model, f"{os.getcwd()}/trained_model.pt")
 
     plt.plot(loss_tracker, '-')
