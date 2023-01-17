78a2358ec722181865dcc2ea000a397c9cacf471
DCZ <dennischenxi@gmail.com>
2023-01-17 16:50:56 +0100

    codecov

--- tests/test_model.py
+++ tests/test_model.py
@@ -11,20 +11,20 @@ class TestClass:
             "Model gives wrong dimensions"
 
     def test_error_on_4D_shape(self):
-        with pytest.raises(ValueError, match="Expected input to a 4D tensor"):
+        with pytest.raises(ValueError, match=r"Expected input to a 4D tensor"):
             self.model(torch.rand(1, 224, 224)).shape == (1, 10)
 
     def test_error_on_shape_1(self):
         with pytest.raises(
-                ValueError, match="Expected dim 1 of sample to have shape {3}"):
+                ValueError, match=r"Expected dim 1 of sample to have shape \{3\}"):
             self.model(torch.rand(1, 6, 224, 224)).shape == (1, 10)
 
     def test_error_on_shape_2(self):
         with pytest.raises(
-                ValueError, match="Expected dim 2 of sample to have shape {224}"):
+                ValueError, match=r"Expected dim 2 of sample to have shape \{224\}"):
             self.model(torch.rand(1, 3, 6, 224)).shape == (1, 10)
 
     def test_error_on_shape_3(self):
         with pytest.raises(
-                ValueError, match="Expected dim 3 of sample to have shape {224}"):
+                ValueError, match=r"Expected dim 3 of sample to have shape \{224\}"):
             self.model(torch.rand(1, 3, 224, 6)).shape == (1, 10)
