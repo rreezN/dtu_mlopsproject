44608f65ed0f8038f7b317f3d10546ac34fd746e
DCZ <dennischenxi@gmail.com>
2023-01-17 16:32:51 +0100

    reverting codecov

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,26 +7,30 @@ class TestClass:
     model = MyAwesomeConvNext("convnext_atto", False, 3, 10)
 
     def test_model(self):
-        assert self.model(torch.rand(1, 3, 224, 224)).shape == (
-            1,
-            10,
-        ), "Model gives wrong dimensions"
+        assert self.model(torch.rand(1, 3, 224, 224)).shape == (1, 10,),\
+            "Model gives wrong dimensions"
+
+
 
     def test_error_on_wrong_shape(self):
         with pytest.raises(ValueError, match="Expected input to a 4D tensor"):
             self.model(torch.rand(1, 224, 224)).shape == (1, 10)
 
         with pytest.raises(
-            ValueError, match="Expected dim 1 of sample to have shape {3}"
+            ValueError, match="Expected each sample to have shape {3, 224, 224}"
         ):
             self.model(torch.rand(1, 6, 224, 224)).shape == (1, 10)
-
-        with pytest.raises(
-                ValueError, match="Expected dim 2 of sample to have shape {224}"
-        ):
             self.model(torch.rand(1, 3, 6, 224)).shape == (1, 10)
-
-        with pytest.raises(
-                ValueError, match="Expected dim 3 of sample to have shape {224}"
-        ):
             self.model(torch.rand(1, 3, 224, 6)).shape == (1, 10)
+        #
+        # with pytest.raises(
+        #         ValueError, match="Expected dim 1 of sample to have shape {3}"):
+        #     self.model(torch.rand(1, 6, 224, 224)).shape == (1, 10)
+        #
+        # with pytest.raises(
+        #         ValueError, match="Expected dim 2 of sample to have shape {224}"):
+        #     self.model(torch.rand(1, 3, 6, 224)).shape == (1, 10)
+        #
+        # with pytest.raises(
+        #         ValueError, match="Expected dim 3 of sample to have shape {224}"):
+        #     self.model(torch.rand(1, 3, 224, 6)).shape == (1, 10)
