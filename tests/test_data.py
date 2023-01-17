b69f0b3ea8aa15fdd0b8a2eac5cf8de6bf3d25e0
DCZ <dennischenxi@gmail.com>
2023-01-17 16:09:11 +0100

    improved code coverage

--- tests/test_data.py
+++ tests/test_data.py
@@ -17,9 +17,6 @@ class dataset(Dataset):
         self.data = images
         self.labels = labels
 
-    def __getitem__(self, item):
-        return self.data[item].float(), self.labels[item]
-
     def __len__(self):
         return len(self.data)
 
